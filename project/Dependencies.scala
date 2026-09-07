cd1e38124c9a307b87fe76aa239575da7303e0c2
Scala Steward <scala_steward@virtuslab.com>
2026-09-07 01:46:16 +0000

    Update fs2-io to 3.14.0

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -18,12 +18,12 @@ object Dependencies {
 
   lazy val `avro-exporter` = Seq(
     "io.janstenpickle" %% "trace4cats-core" % "0.14.0",
-    "co.fs2"           %% "fs2-io"          % "3.2.14"
+    "co.fs2"           %% "fs2-io"          % "3.14.0"
   )
 
   lazy val `avro-server` = Seq(
     "org.typelevel" %% "log4cats-slf4j" % "2.4.0",
-    "co.fs2"        %% "fs2-io"         % "3.2.14"
+    "co.fs2"        %% "fs2-io"         % "3.14.0"
   )
 
 }
